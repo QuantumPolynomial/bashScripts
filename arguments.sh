f34c977c4af7493666175dc19ad2771a54bca23e
Henrik <henrik.ahlkvist@gmail.com>
2016-10-02 16:25:50 +0200

    Added arguments script

---
+++ arguments.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: arguments.sh
+# 
+#         USAGE: ./arguments.sh 
+# 
+#   DESCRIPTION: A script that will take arguments and return the opposite 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: Bash
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Henrik Ahlkvist(henrik.ahlkvist@gmail.com) 
+#  ORGANIZATION: 
+#       CREATED: 2016-10-02 16:24
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
